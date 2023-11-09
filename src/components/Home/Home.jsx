75cc87c5bd3ca5d3a002158c059b3d3378cbb57b
Joseph Oppong <josephoppong253@gmail.com>
2023-11-09 07:30:10 +0000

    portfolio added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Home.css";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Twitter from "../../img/twitter.png";
+import BannerImage from "../../img/bg2.png";
+import UICard from "../../img/uicard.jpg";
+import WebCard from "../../img/webcard.jpg";
+const Home = () => {
+  return (
+    <Container className="mt-5">
+      <div className="row">
+        <div className="col-md-5">
+          <div className="h-wrapper">
+            <div className="h-left">
+              <div className="h-name">
+                <h1 className="mb-4">
+                  Hello! I Am <br />
+                  <span>Oppong Ansah Joseph</span>
+                </h1>
+                <p className="mb-4">
+                  A seasoned UI/UX and front-end developer known for
+                  consistently delivering top-tier web design work characterized
+                  by excellence and quality.
+                </p>
+              </div>
+              <button type="button">Hire Me</button>
+
+              <div className="icons">
+                <img src={Github} alt="Github" />
+                <img src={LinkedIn} alt="LinkedIn" />
+                <img src={Twitter} alt="Twitter" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-7">
+          <div className="h-wrapper">
+            <div className="h-right">
+              <div className="h-banner">
+                <img src={BannerImage} alt="bannerImage" />
+              </div>
+              <div className="h-card">
+                <img className="card1" src={UICard} alt="Card1" />
+                <img className="card2" src={WebCard} alt="Card2" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import './Navbar.css'
+
+function NavigationBar() {
+  return (
+    <>
+      <Navbar expand="lg" className="n-wrapper my-3">
+        <Container>
+          <Navbar.Brand href="#home" className="n-logo">Joseph.Code</Navbar.Brand>
+          <span>Toggler</span>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="n-links ms-auto mx-4 fs-5">
+              <Nav.Link href="#home" className="text-black">Home</Nav.Link>
+              <Nav.Link href="#link" className="text-black">Services</Nav.Link>
+              <Nav.Link href="#link" className="text-black">Experience</Nav.Link>
+              <Nav.Link href="#link" className="text-black">Portfolio</Nav.Link>
+              <Nav.Link href="#link" className="text-black">Team</Nav.Link>
+            </Nav>
+            <button type="button n-button ">Contact</button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavigationBar;
